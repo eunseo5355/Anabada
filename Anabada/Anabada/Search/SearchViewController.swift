d6b13dad255fbe743759f1fea1f22c02fabc779f
eunseo5355 <eunseo5650525@gmail.com>
2023-08-16 04:13:23 +0900

    [FEAT] 검색창이 비어있을때 전체 게시물이 다 뜨도록 수정

--- Anabada/Anabada/Search/SearchViewController.swift
+++ Anabada/Anabada/Search/SearchViewController.swift
@@ -8,26 +8,30 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    //MARK: - @IBOutlet
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var postTableView: UITableView!
     
+    // MARK: - Properties
+    
     let dataManager = DataManager.shared
+    
     var filteredPostData: [PostData] = []
-    var postDataTitle: [String] = []
-    var isFiltering = false
+    var didFilter = false
+    
+    //MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setTableView()
         setSearchBar()
-        
-        for data in dataManager.postData {
-            postDataTitle.append(data.title)
-        }
     }
     
-    func setTableView() {
+    // MARK: - Custom Method
+    
+    private func setTableView() {
         postTableView.delegate = self
         postTableView.dataSource = self
         
@@ -35,23 +39,25 @@ class SearchViewController: UIViewController {
         postTableView.register(nib, forCellReuseIdentifier: PostTableViewCell.identifier)
     }
     
-    func setSearchBar() {
+    private func setSearchBar() {
         searchBar.delegate = self
         searchBar.showsCancelButton = false
     }
 
 }
 
+//MARK: - UITableViewDataSource
+
 extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return isFiltering ? filteredPostData.count : dataManager.postData.count
+        return didFilter ? filteredPostData.count : dataManager.postData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = postTableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier) as? PostTableViewCell
         else { return UITableViewCell() }
         
-        if isFiltering {
+        if didFilter {
             cell.bind(filteredPostData[indexPath.row])
         } else {
             cell.bind(dataManager.postData[indexPath.row])
@@ -62,19 +68,24 @@ extension SearchViewController: UITableViewDataSource {
     
 }
 
+//MARK: - UITableViewDelegate
+
 extension SearchViewController: UITableViewDelegate {
     
 }
 
+//MARK: - UISearchBarDelegate
+
 extension SearchViewController: UISearchBarDelegate {
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        isFiltering = true
-        postTableView.reloadData()
-    }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard let text = searchBar.text?.lowercased() else { return }
-        filteredPostData = dataManager.postData.filter { $0.title.localizedCaseInsensitiveContains(text) }
+        if searchBar.text == "" {
+            didFilter = false
+        } else {
+            didFilter = true
+            guard let text = searchBar.text?.lowercased() else { return }
+            filteredPostData = dataManager.postData.filter { $0.title.localizedCaseInsensitiveContains(text) }
+        }
         postTableView.reloadData()
     }
     
@@ -89,7 +100,7 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.text = ""
         searchBar.resignFirstResponder()
-        isFiltering = false
+        didFilter = false
         postTableView.reloadData()
     }
     
