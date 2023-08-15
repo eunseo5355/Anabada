7a4b7318fd25f99d882b0e5e027ec3d30b27e73f
eunseo5355 <eunseo5650525@gmail.com>
2023-08-15 20:18:53 +0900

    [FEAT] 검색 구현

--- Anabada/Anabada/Search/PostTableViewCell.swift
+++ Anabada/Anabada/Search/PostTableViewCell.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class PostTableViewCell: UITableViewCell {
+    
+    static let identifier = "PostTableViewCell"
 
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
@@ -21,6 +23,13 @@ class PostTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func bind(_ postData: PostData) {
+        titleLabel.text = postData.title
+        timeLabel.text = "30분전"
+        bigCategoryLabel.text = "빌려줌"
+        commentCountLabel.text = "\(postData.comments.count)"
     }
     
 }
--- Anabada/Anabada/Search/SearchViewController.swift
+++ Anabada/Anabada/Search/SearchViewController.swift
@@ -9,21 +9,88 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var postTableView: UITableView!
+    
+    let dataManager = DataManager.shared
+    var filteredPostData: [PostData] = []
+    var postDataTitle: [String] = []
+    var isFiltering = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setTableView()
+        setSearchBar()
+        
+        for data in dataManager.postData {
+            postDataTitle.append(data.title)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setTableView() {
+        postTableView.delegate = self
+        postTableView.dataSource = self
+        
+        let nib = UINib(nibName: PostTableViewCell.identifier, bundle: nil)
+        postTableView.register(nib, forCellReuseIdentifier: PostTableViewCell.identifier)
     }
-    */
+    
+    func setSearchBar() {
+        searchBar.delegate = self
+        searchBar.showsCancelButton = false
+    }
 
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isFiltering ? filteredPostData.count : dataManager.postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = postTableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier) as? PostTableViewCell
+        else { return UITableViewCell() }
+        
+        if isFiltering {
+            cell.bind(filteredPostData[indexPath.row])
+        } else {
+            cell.bind(dataManager.postData[indexPath.row])
+        }
+        
+        return cell
+    }
+    
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        isFiltering = true
+        postTableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard let text = searchBar.text?.lowercased() else { return }
+        filteredPostData = dataManager.postData.filter { $0.title.localizedCaseInsensitiveContains(text) }
+        postTableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        guard let text = searchBar.text?.lowercased() else { return }
+        filteredPostData = dataManager.postData.filter { $0.title.localizedCaseInsensitiveContains(text) }
+        postTableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        isFiltering = false
+        postTableView.reloadData()
+    }
+    
 }
