0e71f5b04740ca414a395e3affaa0be631c971ea
eunseo5355 <eunseo5650525@gmail.com>
2023-08-16 03:49:38 +0900

    [FEAT] 현재 시간과 게시물 올린 시간 차이를 계산하는 메소드 구현

--- Anabada/Anabada/Search/PostTableViewCell.swift
+++ Anabada/Anabada/Search/PostTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class PostTableViewCell: UITableViewCell {
     
     static let identifier = "PostTableViewCell"
-
+    
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
@@ -20,15 +20,38 @@ class PostTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
+    private func calculateTimeAgo(from dateString: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let date = dateFormatter.date(from: dateString) ?? Date()
+        
+        let currentTime = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.minute, .hour, .day, .month], from: date, to: currentTime)
+        
+        if let months = components.month, months > 0 {
+            return "\(months)달 전"
+        } else if let days = components.day, days > 0 {
+            return "\(days)일 전"
+        } else if let hours = components.hour, hours > 0 {
+            return "\(hours)시간 전"
+        } else if let minutes = components.minute, minutes > 0 {
+            return "\(minutes)분 전"
+        } else {
+            return "방금 전"
+        }
+    }
+    
+    
     func bind(_ postData: PostData) {
         titleLabel.text = postData.title
-        timeLabel.text = "30분전"
-        bigCategoryLabel.text = "빌려줌"
+        timeLabel.text = calculateTimeAgo(from: postData.date)
+        bigCategoryLabel.text = postData.bigCategory
         commentCountLabel.text = "\(postData.comments.count)"
     }
     
