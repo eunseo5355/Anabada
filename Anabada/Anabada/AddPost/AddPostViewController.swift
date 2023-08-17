02596c9535ab228d011518c3905dccd34a0d2baf
= <=>
2023-08-17 14:45:32 +0900

    [FEAT]글쓰기, Home 페이지 이미지 처리 전 단계

--- Anabada/Anabada/AddPost/AddPostViewController.swift
+++ Anabada/Anabada/AddPost/AddPostViewController.swift
@@ -8,21 +8,103 @@
 import UIKit
 
 class AddPostViewController: UIViewController {
-
     
-    @IBOutlet var categoryButton: UIButton!
+    
+    
+    @IBOutlet var borrow: UIButton!
+    
+    @IBOutlet var lend: UIButton!
+    
+    var dataManager = DataManager.shared
+    
+    var buttonToggle = true
+    
+    var doneButtonToggle = false
+    
+    @IBOutlet var titleTrailingView: UIView!
+    
+    @IBOutlet var titleTextView: UITextView!
+    
+    @IBOutlet var contentTrailingView: UIView!
+    
+    @IBOutlet var contentTextView: UITextView!
+    
+    @IBOutlet var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleTextView.delegate = self
+        contentTextView.delegate = self
         setUpUi()
+        
+    }
+    
+    private func setUpUi(){
+        setUpButton(state:buttonToggle)
+        setUpTrailingView()
+    }
+    
+    private func setUpButton(state:Bool){
+        
+        borrow.layer.borderWidth = 1
+        borrow.layer.borderColor = UIColor.systemGreen.cgColor
+        borrow.layer.cornerRadius = 15
+        lend.layer.borderWidth = 1
+        lend.layer.borderColor = UIColor.systemGreen.cgColor
+        lend.layer.cornerRadius = 15
+        
+        UIView.animate(withDuration: 0.3){
+            self.borrow.backgroundColor = state == true ? .systemGreen : .clear
+            self.borrow.tintColor = state == true ? .white : .gray
+            self.lend.backgroundColor = state == true ? .clear : .systemGreen
+            self.lend.tintColor = state == true ? .gray : .white
+            self.doneButton.tintColor = self.doneButtonToggle ? UIColor.systemGreen : UIColor.systemGray5
+        }
+        
+
+        
+        borrow.addTarget(self, action: #selector(borrowButtonTapped(sender:)), for: .touchUpInside)
+        lend.addTarget(self, action: #selector(lendButtonTapped(sender:)), for: .touchUpInside)
+        doneButton.addTarget(self, action: #selector(doneButtonTapped(sender:)), for: .touchUpInside)
 
     }
     
-    func setUpUi(){
-
-
+    @objc func borrowButtonTapped(sender:UIButton){
+        buttonToggle = true
+        setUpButton(state: buttonToggle)
     }
-
-
+    @objc func lendButtonTapped(sender:UIButton){
+        buttonToggle = false
+        setUpButton(state: buttonToggle)
+    }
     
+    @objc func doneButtonTapped(sender:UIButton){
+        if doneButtonToggle{
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            let dateString = formatter.string(from: Date())
+            let data = PostData(id: "", title: titleTextView.text, image: "", date: dateString, comments: [], bigCategory: buttonToggle ? "필요해요" : "빌려드려요", smallCategory: "", content: contentTextView.text, nickName: "")
+            dataManager.addNewPost(newPost: data)
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    private func setUpTrailingView(){
+        titleTrailingView.layer.cornerRadius = 15
+        titleTrailingView.layer.borderWidth = 1
+        titleTrailingView.layer.borderColor = UIColor.systemGray5.cgColor
+        contentTrailingView.layer.cornerRadius = 15
+        contentTrailingView.layer.borderWidth = 1
+        contentTrailingView.layer.borderColor = UIColor.systemGray5.cgColor
+    }
+}
+
+extension AddPostViewController: UITextViewDelegate{
+    func textViewDidChange(_ textView: UITextView) {
+        
+        if titleTextView.text != "" && contentTextView.text != ""{doneButtonToggle = true} else {
+            doneButtonToggle = false
+        }
+        setUpButton(state: buttonToggle)
+    }
 }
--- Anabada/Anabada/Home/HomeViewController.swift
+++ Anabada/Anabada/Home/HomeViewController.swift
@@ -13,56 +13,116 @@ class HomeViewController: UIViewController {
     
     @IBOutlet var addNewPostButton: UIButton!
     
-    var dataManager = DataManager.shared
+    private var dataManager = DataManager.shared
     
-    @IBOutlet var leftButton: UIButton!
+    @IBOutlet var menuButton: UIButton!
+    
+    private var state = "total"
+    
+    override func viewWillAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         myTableView.dataSource = self
         setUpUi()
         self.navigationController?.navigationBar.topItem?.title = ""
+        let nib = UINib(nibName: PostTableViewCell.identifier, bundle: nil)
+        myTableView.register(nib, forCellReuseIdentifier: PostTableViewCell.identifier)
     }
     
-    func setUpUi() {
-        makeNaviLeftButton()
+    private func setUpUi() {
+        makeNaviLeftButton(state: "전체")
         setUpAddNewPostButton()
     }
     
-    func setUpAddNewPostButton() {
+    private func setUpAddNewPostButton() {
         addNewPostButton.setTitle("글쓰기", for: .normal)
         addNewPostButton.addTarget(self, action: #selector(addButtonTapped(sender:)), for: .touchUpInside)
         addNewPostButton.layer.cornerRadius = 20
     }
     
-    @objc func addButtonTapped(sender: UIButton) {
-        performSegue(withIdentifier: "AddPostViewController", sender: nil)
-        print("hi")
+    private func makeNaviLeftButton(state:String) {
+        
+        let total = UIAction(title: "전체", handler: { _ in
+            self.state = "total"
+            self.myTableView.reloadData()
+            self.makeNaviLeftButton(state: "전체")
+            self.menuButtonDownAnimate(sender: self.menuButton)
+        })
+        
+        let borrow = UIAction(title: "필요해요", handler: { _ in
+            self.state = "borrow"
+            self.myTableView.reloadData()
+            self.makeNaviLeftButton(state: "필요해요")
+            self.menuButtonDownAnimate(sender: self.menuButton)
+        })
+        
+        let lend = UIAction(title: "빌려드려요", handler: { _ in
+            self.state = "lend"
+            self.myTableView.reloadData()
+            self.makeNaviLeftButton(state: "빌려드려요")
+            self.menuButtonDownAnimate(sender: self.menuButton)
+        })
+        
+        let cancel = UIAction(title: "취소", attributes: .destructive, handler: { _ in
+            self.menuButtonDownAnimate(sender: self.menuButton)
+        })
+        let menu = UIMenu(identifier: nil, options: .displayInline, children: [total, borrow, lend, cancel])
+
+        menuButton.setTitle(state, for: .normal)
+        var newSize = menuButton.intrinsicContentSize
+        newSize.width += 10
+        menuButton.frame.size = newSize
+        menuButton.addTarget(self, action: #selector(menuButtonTapped(sender:)), for: .touchDown)
+        menuButton.menu = menu
+        menuButton.showsMenuAsPrimaryAction = true
     }
     
-    func makeNaviLeftButton() {
-        let total = UIAction(title: "전체", handler: { _ in print("전체Tapped") })
-        let borrow = UIAction(title: "필요해요", handler: { _ in print("필요해요Tapped") })
-        let lend = UIAction(title: "빌려드려요", handler: { _ in print("빌려드려요Tapped") })
-        let cancel = UIAction(title: "취소", attributes: .destructive, handler: { _ in print("취소") })
-        let menu = UIMenu(image: UIImage(systemName: "heart.fill"), identifier: nil, options: .displayInline, children: [total, borrow, lend, cancel])
-
-        leftButton.menu = menu
-        leftButton.transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
-        leftButton.titleLabel?.transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
-        leftButton.imageView?.transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
-        leftButton.showsMenuAsPrimaryAction = true
+    @objc func addButtonTapped(sender: UIButton) {
+        performSegue(withIdentifier: "AddPostViewController", sender: nil)
+    }
+    
+    @objc func menuButtonTapped(sender: UIButton) {
+        menuButtonUpAnimate(sender: menuButton)
+    }
+    
+    private func menuButtonDownAnimate(sender: UIButton) {
+        UIView.transition(with: sender, duration: 0.3, options: .transitionFlipFromBottom, animations: {
+            sender.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        }, completion: nil)
+    }
+    
+    private func menuButtonUpAnimate(sender: UIButton) {
+        UIView.transition(with: sender, duration: 0.3, options: .transitionFlipFromTop, animations: {
+            sender.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+        }, completion: nil)
     }
 }
 
 extension HomeViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        if state == "borrow"{
+            return dataManager.postData.filter{$0.bigCategory == "필요해요"}.count
+        } else if state == "lend"{
+            return dataManager.postData.filter{$0.bigCategory == "빌려드려요"}.count
+        } else {
+            return dataManager.postData.count
+        }
+
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = myTableView.dequeueReusableCell(withIdentifier: "myTableViewCell", for: indexPath)
+        guard let cell = myTableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier) as? PostTableViewCell else { return UITableViewCell() }
+        if state == "borrow"{
+            cell.bind(dataManager.postData.filter{$0.bigCategory == "필요해요"}[indexPath.row])
+        } else if state == "lend"{
+            cell.bind(dataManager.postData.filter{$0.bigCategory == "빌려드려요"}[indexPath.row])
+        } else {
+            cell.bind(dataManager.postData[indexPath.row])
+        }
         return cell
     }
 }
